7628981071021fe71387ddc47bd677eba63032d6
Rasyidi Office <rasyidialwee@gmail.com>
2024-04-29 13:58:37 +0800

    added controller and create basic list

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,10 @@
 <?php
 
+use App\Http\Controllers\CatController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', function () {
-    $name = 'Rasyidi Alwee';
-
-    return view('hello', compact('name'));
-});
+Route::get('/', [CatController::class, 'index']);
