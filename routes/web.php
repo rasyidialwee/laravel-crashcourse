8149ec7b184c691c9d610ee8714e49e13d2c200b
Rasyidi Office <rasyidialwee@gmail.com>
2024-04-29 13:39:15 +0800

    Add hello route and pass name variable to hello view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'Rasyidi Alwee';
+
+    return view('hello', compact('name'));
 });
